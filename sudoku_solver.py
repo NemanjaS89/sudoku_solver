9d5b17d6b82e624f68c9deaa4e0792bfda25c92d
NemanjaS89 <simic.nemanja1989@gmail.com>
2019-11-03 22:57:44 +0100

    redone the algorithm

--- sudoku_solver.py
+++ sudoku_solver.py
@@ -1,70 +1,83 @@
-board = [
-    [7, 8, 0, 4, 0, 0, 1, 2, 0],
-    [6, 0, 0, 0, 7, 5, 0, 0, 9],
-    [0, 0, 0, 6, 0, 1, 0, 7, 8],
-    [0, 0, 7, 0, 4, 0, 2, 6, 0],
-    [0, 0, 1, 0, 5, 0, 9, 3, 0],
-    [9, 0, 4, 0, 6, 0, 0, 0, 5],
-    [0, 7, 0, 3, 0, 0, 0, 1, 2],
-    [1, 2, 0, 0, 0, 7, 4, 0, 0],
-    [0, 4, 9, 2, 0, 6, 0, 0, 7]
+import cv2
+
+
+
+
+
+
+array = [
+        [0,0,0,0,0,9,6,3,0],
+        [0,4,0,6,0,0,8,2,0],
+        [0,0,0,0,0,4,0,1,0],
+        [0,0,8,9,0,0,0,0,0],
+        [2,1,0,0,3,0,0,9,7],
+        [0,0,0,0,0,5,3,0,0],
+        [0,7,0,4,0,0,0,0,0],
+        [0,6,9,0,0,3,0,7,0],
+        [0,2,3,5,0,0,0,0,0]
 ]
 
-def print_board(sudoku):
-
-    for i in range(len(sudoku)):
+def print_table(sudoku):
+    for i in range(9):
         if i % 3 == 0 and i != 0:
-            print("-----------------------")
-
-        for j in range(len(sudoku[0])):
+            print('--------------------')
+        for j in range(9):
             if j % 3 == 0 and j != 0:
-                print(" | ", end="")
-
+                print('| ', end='')
             if j == 8:
                 print(str(sudoku[i][j]))
             else:
-                print(str(sudoku[i][j]) + " ", end="")
+                print(str(sudoku[i][j]) + " ", end='')
 
-def find_empty(sudoku):
 
-    for i in range(len(sudoku)):
-        for j in range(len(sudoku[0])):
+def free_square(sudoku, k):
+    for i in range(9):
+        for j in range(9):
             if sudoku[i][j] == 0:
-                return (i, j) #row, col
+                k[0] = i
+                k[1] = j
+                return True
+    return False
 
-    return None
 
-def valid(sudoku, num, pos):
-    #row check
-    for i in range(len(sudoku[0])):
-        if sudoku[pos[0]][i] == num and pos[1] != i:
-            return False
-    #column check
-    for i in range(len(sudoku)):
-        if sudoku[i][pos[1]] == num and pos[0] != i:
-            return False
-    #box check
-    box_x = pos[1] // 3
-    box_y = pos[0] // 3
+def row_collision(sudoku, num, row):
+    for i in range(9):
+        if sudoku[row][i] == num:
+            return True
+    return False
 
-    for i in range(box_y * 3, box_y * 3 + 3):
-        for j in range(box_x * 3, box_x * 3 + 3):
-            if sudoku[i][j] == num and (i, j) != pos:
-                return False
 
-    return True
+def column_collision(sudoku, num, col):
+    for i in range(9):
+        if sudoku[i][col] == num:
+            return True
+    return False
+
+
+def square_collision(sudoku, num, row, col):
+    for i in range(3):
+        for j in range(3):
+            if sudoku[i + row][j + col] == num:
+                return True
+    return False
+
+
+def valid_square(sudoku, num, row, col):
+    return not row_collision(sudoku, num, row) and not column_collision(sudoku, num, col) and not square_collision(sudoku, num, row - row % 3, col - col % 3)
 
 def solve(sudoku):
 
-    find = find_empty(sudoku)
-    if not find:
-        return True
-    else:
-        row, col = find
+    k = [0, 0]
 
-    for i in range(1, 10):
-        if valid(sudoku, i, (row, col)):
-            sudoku[row][col] = i
+    if not free_square(sudoku, k):
+        return True
+
+    row = k[0]
+    col = k[1]
+
+    for num in range(1, 10):
+        if valid_square(sudoku, num, row, col):
+            sudoku[row][col] = num
 
             if solve(sudoku):
                 return True
@@ -73,8 +86,5 @@ def solve(sudoku):
 
     return False
 
-print_board(board)
-solve(board)
-print("")
-print("")
-print_board(board)
+solve(array)
+print_table(array)
